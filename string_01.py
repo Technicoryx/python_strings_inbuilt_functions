78892d043c6c6d65affe3bd4906ba0162c5d6604
Technicoryx <technicoryx@gmail.com>
2021-07-19 09:37:42 +0530

    v commit-02

--- string_01.py
+++ string_01.py
@@ -2,9 +2,15 @@
 functions in a string"""
 
 string_01 = "Python is a good language"
-string_01.upper()
+#string_02 = string_01.upper()
+#print(string_02)
+#print(string_01)
 #O/P: PYTHON IS A EASY TO
-string_01.lower()
+#string_03 = string_01.lower()
+#print(string_03);
+#print(string_01);
 #O/P: python is easy to learn
-string_01.capitalize()
+string_06 = "232 the Python"
+string_05 = string_06.capitalize();
+print(string_05)
 #/P: Python is easy to learn
